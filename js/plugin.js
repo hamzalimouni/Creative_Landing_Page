03e28e3c6e70806ca5a678bbe7627b153be16392
ELYAMOUNI <hamzalimouni9@gmail.com>
2023-01-21 17:30:43 +0100

    add: all files

---
+++ js/plugin.js
@@ -0,0 +1,10 @@
+let img = document.querySelector('.imgs');
+let container = document.querySelector('.container');
+
+function phones(src){
+    img.src = src;
+}
+
+function colors(color){
+    container.style.backgroundColor = color;
+}
